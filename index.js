216edaa36e1062c98e6c274a2fbf34ea1db8faad
shubham ghuge <>
2021-05-22 12:10:47 +0000

    checking token

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get('/user',(req,res,next)=>{
-  console.log("requested resource");
-  next();
-})
-
 app.get('/', (req, res) => {
   res.send("helloo")
 });
 
+app.get('/user', (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    console.log(`${token} user has requested`);
+    next();
+  } else {
+    res.status(401).json({ msg: "error" })
+  }
+})
+
 app.get('/user', (req, res) => {
-  res.json({ name: "shubham", age: 21,pincode:400066 })
+  res.json({ name: "shubham", age: 21, pincode: 400066 })
 });
 
 app.listen(3000, () => {
